688081a6a38709018fe398e77c9de2010986dd80
이정우 <ijeong-u@ijeong-uui-MacBookAir.local>
2026-01-25 16:47:54 -0500

    fix: explicit google id/secret

--- frontend/auth.ts
+++ frontend/auth.ts
@@ -2,7 +2,12 @@ import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
-    providers: [Google],
+    providers: [
+        Google({
+            clientId: process.env.AUTH_GOOGLE_ID,
+            clientSecret: process.env.AUTH_GOOGLE_SECRET,
+        }),
+    ],
     debug: true, // 에러 로그를 자세히 보기 위함
     trustHost: true, // Vercel 배포 시 호스트 신뢰 설정 (필수)
     secret: process.env.AUTH_SECRET, // 명시적으로 시크릿 지정

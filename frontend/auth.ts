3b349cc4dc3cf52d6ea6d7206afbaa03e3b72316
이정우 <ijeong-u@ijeong-uui-MacBookAir.local>
2026-01-25 23:10:48 -0500

    fix: explicit auth config

--- frontend/auth.ts
+++ frontend/auth.ts
@@ -2,6 +2,13 @@ import NextAuth from "next-auth"
 import Google from "next-auth/providers/google"
 
 export const { handlers, auth, signIn, signOut } = NextAuth({
-    providers: [Google],
+    providers: [
+        Google({
+            clientId: process.env.AUTH_GOOGLE_ID,
+            clientSecret: process.env.AUTH_GOOGLE_SECRET,
+        })
+    ],
     trustHost: true,
+    secret: process.env.AUTH_SECRET,
+    debug: true,
 })
